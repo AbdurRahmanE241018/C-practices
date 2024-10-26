3eb440a104bbdabf7922dd2c024c685028155deb
Abdur Rahman <159477884+AbdurRahmanE241018@users.noreply.github.com>
2024-10-26 08:16:32 -0500

    Add files via upload

---
+++ 01-PlayWithPrintf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    /*
+    This is a program which will print a simple hello world in C.
+    I want to practice my C programming.
+    */
+
+    printf("Hello world in C.This is first line.");
+    //printf("This is second line.\n");
+    //printf("This is third line.");
+    //sleep(10000);
+
+    return 0;
+}
