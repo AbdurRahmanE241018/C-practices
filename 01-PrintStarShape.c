7a994601d0e8f6dfa1870a4e54c5a4cb7c53fdb0
Abdur Rahman <159477884+AbdurRahmanE241018@users.noreply.github.com>
2024-10-26 08:18:22 -0500

    Added by me

---
+++ 01-PrintStarShape.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    /*
+    This is a program to print a star shape.
+    */
+    printf("*\n");
+    printf("**\n");
+    printf("***\n");
+    printf("****\n");
+    printf("*****\n");
+    printf("******");
+
+    return 0;
+}
